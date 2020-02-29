31f8518659a9ee0678f5371a230dfa4faa12435e
Venkatraman <vijay4313@gmail.com>
2020-02-29 17:53:16 -0500

    Added summary stats function

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -57,3 +57,51 @@ def get_loss_fn(loss_name):
         return nn.CrossEntropyLoss()
     else:
         raise ValueError('Unknown loss function')
+
+
+class SummaryStatistics(object):
+    def init(self, n_classes=4):
+        self.n_classes = 4
+        self.reset()
+    
+    def reset(self):
+        self.confusion_matrix = np.zeros((self.n_classes, self.n_classes))
+
+    def update(self, true_labels, pred_labels):
+        if len(pred_labels.shape) > 1:
+            pred_labels = np.argmax(pred_labels, axis=-1)
+        conf_matrix = np.bincount(
+                self.n_classes * true_labels.astype(int) + pred_labels.astype(int), minlength=self.n_classes ** 2
+            ).reshape(self.n_classes, self.n_classes)
+
+        self.confusion_matrix += conf_matrix
+
+    def get_metrics(self):
+        conf_matrix = self.confusion_matrix
+        precision_per_class = np.diag(conf_matrix) / np.sum(conf_matrix, axis=0)
+        recall_per_class = np.diag(conf_matrix) / np.sum(conf_matrix, axis=1)
+        acc_per_class = np.diag(conf_matrix) / (np.sum(conf_matrix, axis=1) + np.sum(conf_matrix, axis=0) - np.diag(conf_matrix))
+        f1_per_class = 2 * precision_per_class * recall_per_class / (precision_per_class + recall_per_class)
+
+        avg_precision = np.nanmean(precision_per_class)
+        avg_recall = np.nanmean(recall_per_class)
+        avg_acc = np.nanmean(acc_per_class)
+        avg_f1 = 2 * avg_precision * avg_recall / (avg_precision + avg_recall) 
+
+        result = {
+            'conf_matrix':  conf_matrix,
+            'stats_per_class':  {
+                'class_precision':  precision_per_class,
+                'class_recall': recall_per_class,
+                'class_accuracy':   acc_per_class,
+                'class_f1': f1_per_class
+            },
+            'avg_stats': {
+                'avg_precision':    avg_precision,
+                'avg_recall':   avg_recall,
+                'avg_accuracy': avg_acc,
+                'avg_f1':   avg_f1
+            }
+        }
+
+        return result
